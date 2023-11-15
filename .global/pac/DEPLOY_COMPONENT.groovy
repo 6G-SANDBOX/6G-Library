f2b9fc4770106f65083e30340f2b57bfc7cbfa54
Jesus Macias Portela <jesus.maciasportela@telefonica.com>
2023-11-15 17:23:26 +0100

    Add sites folder

--- .global/pac/DEPLOY_COMPONENT.groovy
+++ .global/pac/DEPLOY_COMPONENT.groovy
@@ -11,8 +11,8 @@ pipeline {
     string(name: 'TN_ID', defaultValue: '', description: 'TRIAL NETWORK IDENTIFIER')
     string(name: 'LIBRARY_COMPONENT_NAME', defaultValue: '', description: '6G LIBRARY COMPONENT')
     string(name: 'LIBRARY_BRANCH', defaultValue: 'main', description: '6G LIBRARY BRANCH')
+    string(name: 'DEPLOYMENT_SITE', defaultValue: 'uma', description: 'Site ')
     base64File (name: 'FILE', description: 'YAML file that contains variables needed to deploy the component')
-    //string(name: 'TN_TERRAFORM_STATE_URL', defaultValue: 'https://tnlcm.uma/TN/ID/tfstate', description: 'TN Terraform state')
     //string(name: 'TNLCM_CALLBACK', defaultValue: 'https://tnlcm.uma/TN/ID/callback', description: 'URL of the TNLCM to notify result')   
   }
 
@@ -21,6 +21,7 @@ pipeline {
       TN_ID="${params.TN_ID}"
       LIBRARY_COMPONENT_NAME="${params.LIBRARY_COMPONENT_NAME}"
       LIBRARY_BRANCH="${params.LIBRARY_BRANCH}"
+      DEPLOYMENT_SITE="${params.DEPLOYMENT_SITE}"
       FILE_PATH="/tmp/${params.TN_ID}_${params.LIBRARY_COMPONENT_NAME}_vars.yaml"
       OPENNEBULA_API_CREDENTIALS = credentials('OPENNEBULA_API_CREDENTIALS')
       OPENNEBULA_USERNAME = credentials('OPENNEBULA_USERNAME')
@@ -58,7 +59,7 @@ pipeline {
         script {
           sh """ 
           cd ${LIBRARY_COMPONENT_NAME}/private
-          ansible-playbook --extra-vars "tn_id=${TN_ID} component_name=${LIBRARY_COMPONENT_NAME}" manifest.yaml
+          ansible-playbook --extra-vars "tn_id=${TN_ID} component_name=${LIBRARY_COMPONENT_NAME}" deployment_site=${DEPLOYMENT_SITE}" manifest.yaml
           """
           }
       }
