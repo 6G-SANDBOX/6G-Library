87ed071cfc2f0d5a92bb5a0f75f8e32fdc3fd5a0
Álvaro Curto Merino <alvaro.curtomerino@telefonica.com>
2024-03-15 12:02:05 +0100

    Delete unnecesary comments

--- .global/pac/DEPLOY_COMPONENT.groovy
+++ .global/pac/DEPLOY_COMPONENT.groovy
@@ -9,12 +9,12 @@ pipeline {
 
   parameters {
     string(name: 'TN_ID', defaultValue: '', description: 'TRIAL NETWORK IDENTIFIER')
-    string(name: 'LIBRARY_URL', defaultValue: 'https://github.com/6G-SANDBOX/6G-Library', description: '6G LIBRARY REPOSITORY') // NEW. Making the repository an optional parameter makes it easier to test repository forks
+    string(name: 'LIBRARY_URL', defaultValue: 'https://github.com/6G-SANDBOX/6G-Library', description: '6G LIBRARY REPOSITORY')
     string(name: 'LIBRARY_BRANCH', defaultValue: 'main', description: '6G LIBRARY BRANCH')
     string(name: 'LIBRARY_COMPONENT_NAME', defaultValue: '', description: '6G LIBRARY COMPONENT')
     string(name: 'DEPLOYMENT_SITE', defaultValue: 'uma', description: 'Site where deploy')
     base64File (name: 'FILE', description: 'YAML file that contains variables needed to deploy the component')
-    booleanParam(name: 'DEBUG', defaultValue: false, description: 'Enable DEBUG')                 // NEW. New binary parameter to debug the workspace
+    booleanParam(name: 'DEBUG', defaultValue: false, description: 'Enable DEBUG')
     //string(name: 'TNLCM_CALLBACK', defaultValue: 'https://tnlcm.uma/TN/ID/callback', description: 'URL of the TNLCM to notify result')   
   }
 
@@ -25,11 +25,11 @@ pipeline {
       LIBRARY_COMPONENT_NAME="${params.LIBRARY_COMPONENT_NAME}"
       DEPLOYMENT_SITE="${params.DEPLOYMENT_SITE}"
       FILE_PATH="${WORKSPACE}/${params.LIBRARY_COMPONENT_NAME}/private/tnlcm_vars.yaml"
-      // OPENNEBULA_API_CREDENTIALS = credentials('OPENNEBULA_API_CREDENTIALS')     // Useless???
+      OPENNEBULA_API_CREDENTIALS = credentials('OPENNEBULA_API_CREDENTIALS')
       OPENNEBULA_USERNAME = credentials('OPENNEBULA_TNLCM_USERNAME')
       OPENNEBULA_PASSWORD = credentials('OPENNEBULA_TNLCM_PASSWORD')
       OPENNEBULA_ENDPOINT = credentials('OPENNEBULA_ENDPOINT')
-      //OPENNEBULA_FLOW_ENDPOINT = credentials('OPENNEBULA_FLOW_ENDPOINT')
+      OPENNEBULA_FLOW_ENDPOINT = credentials('OPENNEBULA_FLOW_ENDPOINT')
       OPENNEBULA_INSECURE = credentials('OPENNEBULA_INSECURE')
       // MINIO CREDENTIALS
       AWS_ACCESS_KEY_ID = credentials('MINIO_KEY')
@@ -65,7 +65,6 @@ pipeline {
           }
       }
     }
-    // NOTE: If parameter enviromental variables are declared in the "enviroment" stage, it should be unnecesary to declare them again in the ansible-playbook command line
     stage('Stage 2: Run component deployment') {
       steps {
         script {
