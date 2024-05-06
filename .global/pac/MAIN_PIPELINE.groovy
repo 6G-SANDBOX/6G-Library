adb02626915fecd6ddd5081c3d17b09985a909e3
Álvaro Curto Merino <alvaro.curtomerino@telefonica.com>
2024-05-06 19:03:34 +0200

    tn_vxlan progress

--- .global/pac/MAIN_PIPELINE.groovy
+++ .global/pac/MAIN_PIPELINE.groovy
@@ -14,16 +14,22 @@ pipeline {
         string(name: 'ENTITY_NAME', defaultValue: '', description: 'Custom name for the component inside the Trian Network')
         choice(name: 'DEPLOYMENT_SITE', choices: ['uma', 'athens', 'fokus'], description: 'Site where the deployment is being made')
         string(name: 'TNLCM_CALLBACK', defaultValue: 'http://tnlcm-ip:5000/tnlcm/callback/', description: 'URL of the TNLCM to notify the results')
-        string(name: 'LIBRARY_URL', defaultValue: 'https://github.com/6G-SANDBOX/6G-Library', description: '6G-Library Repository URL. Leave it as-is unless you want to test your own fork')
+        string(name: 'LIBRARY_URL', defaultValue: 'https://github.com/6G-SANDBOX/6G-Library', description: '6G-Library Repository HTTPS URL. Leave it as-is unless you want to test your own fork')
         string(name: 'LIBRARY_BRANCH', defaultValue: 'main', description: 'LIBRARY_URL branch to use')
+        string(name: 'SITES_URL', defaultValue: 'git@github.com:6G-SANDBOX/6G-Sandbox-Sites.git', description: '6G-Library-Sites repository SSH URL. Leave it as-is unless you want to test your own fork')
+        string(name: 'SITES_BRANCH', defaultValue: 'main', description: 'SITES_URL branch to use')
         booleanParam(name: 'DEBUG', defaultValue: false, description: 'Enable DEBUG')
         // 'File Parameter' jenkins plugin required: https://plugins.jenkins.io/file-parameters/
         base64File (name: 'FILE', description: 'YAML file that contains variables needed to deploy the component')
     }
 
+Add `SITE_URL` and `SITE_BRANCH` parameters to test forks of the **6G-Sandbox-Sites** repository. 
+
     // Enviromental variables inherited from Jenkins Credentials
     environment {
-        // Github token to access the 6G-Sandbox-Sites repository
+        // URL, branch and github token to clone the 6G-Sandbox-Sites repository
+        SITES_URL="${params.SITES_URL}"
+        SITES_BRANCH="${params.SITES_BRANCH}"
         GITHUB_JENKINS = credentials('GITHUB_JENKINS') 
 
         // Opennebula Terraform Provider envorimental variables https://registry.terraform.io/providers/OpenNebula/opennebula/latest/docs#environment-variables
@@ -34,14 +40,15 @@ pipeline {
         OPENNEBULA_FLOW_ENDPOINT = credentials('OPENNEBULA_FLOW_ENDPOINT')
         OPENNEBULA_INSECURE = credentials('OPENNEBULA_INSECURE')
 
-        ONE_XMLRPC = credentials('ONE_XMLRPC')     // useless? check utility or remove
-        ONE_AUTH = credentials('ONE_AUTH')         // useless? check utility or remove
+        // Values used in OpenNebula CLI commands https://docs.opennebula.io/6.8/management_and_operations/references/cli.html#shell-environment
+        ONE_XMLRPC = credentials('ONE_XMLRPC')
+        ONE_AUTH = credentials('ONE_AUTH')
 
         // AWS Terraform Provider envirmental variables (for the MinIO S3 storage) https://registry.terraform.io/providers/hashicorp/aws/2.54.0/docs#environment-variables
         AWS_ACCESS_KEY_ID = credentials('MINIO_KEY')
         AWS_SECRET_ACCESS_KEY = credentials('MINIO_SECRET')
 
-        //Ansible enviromental variables https://docs.ansible.com/ansible/latest/reference_appendices/config.html
+        // Ansible enviromental variables https://docs.ansible.com/ansible/latest/reference_appendices/config.html
         // ANSIBLE_REMOTE_USER = credentials('ANSIBLE_REMOTE_USER')
         // ANSIBLE_CONNECTION_PASSWORD_FILE = credentials('ANSIBLE_CONNECTION_PASSWORD_FILE')
     }
@@ -65,20 +72,9 @@ pipeline {
             }
         }
 
-        stage('Stage 2: Clone 6G-Sandbox-Sites repository') {
+        stage('Stage 2: Import Jenkins parameters into the workspace') {
             steps {
-                echo 'Stage 2: Clone 6G-Sandbox-Sites repository'
-                dir ("${env.WORKSPACE}/") {
-                    // avoid Injection via interpolation
-                    // sh "git clone https://${GITHUB_JENKINS}@github.com/6G-SANDBOX/6G-Sandbox-Sites.git"
-                    sh('git clone https://$GITHUB_JENKINS@github.com/6G-SANDBOX/6G-Sandbox-Sites.git')
-                }
-            }
-        }
-
-        stage('Stage 3: Import Jenkins parameters into the workspace') {
-            steps {
-                echo 'Stage 3: Load Jenkins parameters into the workspace'
+                echo 'Stage 2: Load Jenkins parameters into the workspace'
                 script{
                     def paramsFile = "${WORKSPACE}/${params.LIBRARY_COMPONENT_NAME}/variables/pipeline_parameters.yaml"
                     def paramsContent = "tn_id: ${params.TN_ID}\n"
@@ -86,15 +82,29 @@ pipeline {
                     paramsContent += "entity_name: ${params.ENTITY_NAME}\n"
                     paramsContent += "deployment_site: ${params.DEPLOYMENT_SITE}\n"
                     paramsContent += "tnlcm_callback: ${params.TNLCM_CALLBACK}\n"
-                    paramsContent += "library_url: ${params.LIBRARY_URL}\n"
-                    paramsContent += "library_branch: ${params.LIBRARY_BRANCH}\n"
                     paramsContent += "debug: ${params.DEBUG}\n"
 
                     writeFile file: paramsFile, text: paramsContent
                 }
             }
         }
-   
+
+        stage('Stage 3: Clone 6G-Sandbox-Sites repository') {
+            steps {
+                echo 'Stage 3: Clone 6G-Sandbox-Sites repository'
+                // dir ("${env.WORKSPACE}/") {
+                //     // avoid Injection via interpolation
+                //     // sh "git clone https://${GITHUB_JENKINS}@github.com/6G-SANDBOX/6G-Sandbox-Sites.git"
+                //     sh('git clone https://$GITHUB_JENKINS@github.com/6G-SANDBOX/6G-Sandbox-Sites.git')
+                // }
+                script {
+                    def gitUrlWithToken = SITES_URL.replace('git@github.com:', "https://${GITHUB_TOKEN}@github.com/")
+                    // Cloning the repository
+                    git branch: SITES_BRANCH url: gitUrlWithToken
+                }
+            }
+        }
+
         stage('Stage 4: Deploy the selected component') {
             steps {
                 echo "Stage 3: Run ansible playbook to deploy ${TN_ID}-${LIBRARY_COMPONENT_NAME}-${ENTITY_NAME} in the ${DEPLOYMENT_SITE} site"
--- changelog.md
+++ changelog.md
@@ -7,6 +7,9 @@
 - Standardized changelog format to fit [keep-a-changelog](https://github.com/olivierlacan/keep-a-changelog/blob/main/CHANGELOG.md) rules.
 - New parameters added to the Jenkinsfile: `ENTITY_NAME`, `TNLCM_CALLBACK`, `LIBRARY_URL` and `LIBRARY_BRANCH`
 - New `.global/hypervisors/` directory replacing `<component>/private/<hypervisor>/prepare.yml` with the steps common to each hypervisor
+- New `.global/cac/deploy_component.yaml` file with ansible tasks common to all components.
+All ansible executions will start from here, to later import its corresponding "component_playbook.yaml" file
+- Minor ansible tasks moved to a `tasks/` directory inside `.global/cac`
 
 ### Changed
 
@@ -35,26 +38,12 @@
 
 
 Yet to be done:
-- verify utility of some pipeline enviroment variables
+- Use Ansible ENVIROMENTAL VALUES to specify the ansible user into the VMs (rootless priviledged user)
 - Change OpenNebula credentials so that `oneadmin` isn't the Jenkins user no more.
 A less privileged user named `jenkins-master` will be the one deploying the components.
 It is the admin of a newly created group called `6G-Sandbox`, where custom users may be added in the future.
-- New `.global/cac/deploy_component.yaml` file with ansible tasks common to all components.
-All ansible executions will start from here, to import the corresponding "component playbook"
-- Delete `.global/cac/mix_variables.yaml` file, and `.global/cac/mix_variables_tasks.yaml` renamed to `load_variables.yaml`
-- Rename `<component>/private/manifest.yaml` to `<component>/private/playbook.yaml`.
-The term manifest is ambiguous an can refer both to the terraform files and to this yaml one.
-The name `playbook.yml` is more clear and would lead to less confusion.
 - Replace Ansible's `amazon.aws.aws_s3`module to a current one like `amazon.aws.s3_object`
-- Add `SITE_URL` and `SITE_BRANCH` parameters to test forks of the **6G-Sandbox-Sites** repository. 
-Remember to change from Jenkins the value of the `GITHUB_JENKINS` credential so that it can access all required repos.
 
-
-
-To be discussed:
-- remove public.yaml/name ??
-- rename public.yaml/metadata/needs
-- optional component dependencies. TNLCM needs aditional logic (not listed in metadata) to order components if for example a VM uses aditional vxlans
 
 
 
