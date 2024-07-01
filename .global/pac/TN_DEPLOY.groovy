a281b7ede23c953ad13899f3ebef14ae5fb3d46e
Álvaro Curto Merino <alvaro.curtomerino@telefonica.com>
2024-07-01 13:42:04 +0200

    decrypt sites

--- .global/pac/TN_DEPLOY.groovy
+++ .global/pac/TN_DEPLOY.groovy
@@ -17,7 +17,7 @@ pipeline {
         string(name: 'LIBRARY_URL', defaultValue: 'https://github.com/6G-SANDBOX/6G-Library.git', description: '6G-Library repository HTTPS URL. Leave it as-is unless you want to test your own fork')
         string(name: 'LIBRARY_BRANCH', defaultValue: 'refs/heads/main', description: 'LIBRARY_URL checkout to use. Valid inputs can be refs/heads/<branchName>, refs/tags/<tagName> or <commitId>. Leave it as-is unless you want to test alternative releases/branches/commits.')
         string(name: 'SITES_URL', defaultValue: 'https://github.com/6G-SANDBOX/6G-Sandbox-Sites.git', description: '6G-Library-Sites repository HTTP URL. Leave it as-is unless you want to test your own fork')
-        string(name: 'SITES_BRANCH', defaultValue: 'main', description: 'SITES_URL checkout to use. Valid inputs can be <branchName>, <tagName> or <commitId>. Leave it as-is unless you want to test alternative releases/branches/commits.')
+        string(name: 'SITES_BRANCH', defaultValue: 'refs/heads/main', description: 'SITES_URL checkout to use. Valid inputs can be <branchName>, <tagName> or <commitId>. Leave it as-is unless you want to test alternative releases/branches/commits.')
         booleanParam(name: 'DEBUG', defaultValue: false, description: 'Enable DEBUG. Files will not be purged after the pipeline execution')
         // 'File Parameter' jenkins plugin required: https://plugins.jenkins.io/file-parameters/
         base64File (name: 'FILE', description: 'YAML file that contains the public variables needed to deploy the component')
