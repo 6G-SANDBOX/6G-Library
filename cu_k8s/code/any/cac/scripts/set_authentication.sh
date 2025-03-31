58f74744ed8e83e0eb76797e77cf2d93877ef6e6
Szymon <s.mecina@is-wireless.com>
2025-03-31 15:56:44 +0200

    Authentication update

--- cu_k8s/code/any/cac/scripts/set_authentication.sh
+++ cu_k8s/code/any/cac/scripts/set_authentication.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 helm registry login registry-1.docker.io -u smecina -p $DOCKERHUB_TOKEN
+kubectl create secret docker-registry regcred --docker-username=smecina --docker-password=$DOCKERHUB_TOKEN
