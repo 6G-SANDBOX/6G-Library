1cf5d39f65b437499aed91b281925ab3183cee4b
Szymon <s.mecina@is-wireless.com>
2025-04-22 11:39:50 +0200

    Add interface scanning

--- cu_k8s/code/any/cac/scripts/set_authentication.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-helm registry login registry-1.docker.io -u smecina -p $DOCKERHUB_TOKEN
-kubectl create secret docker-registry regcred --docker-username=smecina --docker-password=$DOCKERHUB_TOKEN
